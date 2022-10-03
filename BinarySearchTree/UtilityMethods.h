ffe4221768e9c7ae357967763471938b992f3544
Kit Cook <N0900053@my.ntu.ac.uk>
2022-10-03 14:44:11 +0100

    add tests

--- BinarySearchTree/UtilityMethods.h
+++ BinarySearchTree/UtilityMethods.h
@@ -1,6 +1,14 @@
 #pragma once
 class UtilityMethods
 {
-	int Max(int a, int b);
+public:
+	static int Max(int a, int b);
 };
+
+// A utility function to get maximum
+// of two integers
+int UtilityMethods::Max(int a, int b)
+{
+	return a > b ? a : b;;
+}
 
